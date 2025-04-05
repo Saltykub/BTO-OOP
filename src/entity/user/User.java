c038feaad3a93bc2c9df028576a1ce954fa4d3ed
Sean-Yong88 <sean0048@e.ntu.edu.sg>
2025-04-05 18:39:37 +0800

    Add User and UsersApp class
    Can delete UsersApp after testing

--- src/entity/user/User.java
+++ src/entity/user/User.java
@@ -1,5 +1,57 @@
 package entity.user;
 
 public class User {
-    
+    private String userID;
+    private String name;
+    private String hashedPassword;
+    private int age;
+    private MaritalStatus maritalStatus;
+
+    public User(String userID, String name, String hashedPassword, int age, MaritalStatus maritalStatus) {
+        this.userID = userID;
+        this.name = name;
+        this.hashedPassword = hashedPassword;
+        this.age = age;
+        this.maritalStatus = maritalStatus;
+    }
+
+    public String getUserId() {
+        return userID;
+    }
+
+    public void setUserId(String userID) {
+        this.userID = userID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getHashedPassword() {
+        return hashedPassword;
+    }
+
+    public void setHashedPassoword(String hashedPassword) {
+        this.hashedPassword = hashedPassword;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public MaritalStatus getMaritalStatus() {
+        return maritalStatus;
+    }
+
+    public void setMaritalStatus(MaritalStatus maritalStatus) {
+        this.maritalStatus = maritalStatus;
+    }
 }
