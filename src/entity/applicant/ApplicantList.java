654182259414c0ed39efeef1f575c534d7bb0cd0
sathwiik <123555744+sathwiik@users.noreply.github.com>
2025-04-07 14:12:35 +0800

    Update ApplicantList.java

--- src/entity/applicant/ApplicantList.java
+++ src/entity/applicant/ApplicantList.java
@@ -1,2 +1,15 @@
+package entity.project;
+
 public class ApplicantList extends ModelList {
-  
+  private String filePath;
+  private List<Applicant> applicants;
+
+  public ApplicantList() {
+    this.applicants = new ArrayList<>();
+  }
+
+  public String getFilePath() {
+    return filePath;
+  }
+
+}
