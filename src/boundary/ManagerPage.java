bfc2f9b3805ecd39ff23ac43742cb7a315a2b9a4
sathwiik <123555744+sathwiik@users.noreply.github.com>
2025-04-08 11:49:03 +0800

    Update ManagerPage.java

--- src/boundary/ManagerPage.java
+++ src/boundary/ManagerPage.java
@@ -1,5 +1,29 @@
 package boundary;
 
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
 public class ManagerPage {
     
+    public ManagerPage() {
+        // To be implemented
+    }
+    
+    // Page management methods
+    public void viewRegisteredProject() {}
+    public void viewEnquiries() {}
+    public void answerEnquiries() {}
+    public void viewProjectList() {}
+    public void viewApplicantApplicationStatus() {}
+    public void viewRequest() {}
+    public void changeRequestStatus() {}
+    public void changeApplicantApplication() {}
+    public void viewAllEnquiries() {}
+    public void createProject() {}
+    public void editProject() {}
+    public void deleteProject() {}
+    public void toggleVisibility() {}
+    public void viewOfficerRegistrationStatus() {}
+    public void generateReport() {}
 }
