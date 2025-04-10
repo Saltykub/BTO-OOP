3e946f4272ae00a9c1568915c6dae079ac6b5ef0
autoastt <peeranat.kjp@gmail.com>
2025-04-10 22:54:46 +0800

    add officerreg constructor

--- src/entity/request/OfficerRegistration.java
+++ src/entity/request/OfficerRegistration.java
@@ -3,6 +3,11 @@ package entity.request;
 public class OfficerRegistration extends Request {
     private ApprovedStatus registrationStatus;
 
+    public OfficerRegistration(String requestID, RequestType requestType, String userID, String projectID, RequestStatus requestStatus) {
+        super(requestID, requestType, userID, projectID, requestStatus);
+        this.registrationStatus = ApprovedStatus.PENDING;
+    }
+
     public ApprovedStatus getRegistrationStatus() {
         return registrationStatus;
     }
