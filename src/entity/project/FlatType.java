63cac893ba77d62ac4bd43620c681ce7a19e4e10
Sathwiik Sai <sathwiiksai@Sathwiiks-MacBook-Air.local>
2025-04-23 22:13:06 +0800

    project comments

--- src/entity/project/FlatType.java
+++ src/entity/project/FlatType.java
@@ -1,5 +1,8 @@
 package entity.project;
-
+/**
+ * Represents the different types of flats available within housing projects.
+ * Used to categorize units, prices, and applicant eligibility.
+ */
 public enum FlatType {
     TWO_ROOM,
     THREE_ROOM
